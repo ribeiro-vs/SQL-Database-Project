bce72d8468e1aaba86ac278d68ae6e9da679bcac
Vinicius Ribeiro <87951323+ribeiro-vs@users.noreply.github.com>
2022-06-28 00:22:33 -0300

    Create order.sql

---
+++ Tables/order.sql
@@ -0,0 +1,9 @@
+create table "Order" (
+   Id                   int                  identity,
+   OrderDate            datetime             not null default getdate(),
+   OrderNumber          nvarchar(10)         null,
+   CustomerId           int                  not null,
+   TotalAmount          decimal(12,2)        null default 0,
+   constraint PK_ORDER primary key (Id)
+)
+go
