d10aaed0e470282e250d0b5fe53b90027bc91980
Vinicius Ribeiro <87951323+ribeiro-vs@users.noreply.github.com>
2022-06-28 00:24:08 -0300

    Create supplier.sql

---
+++ Tables/supplier.sql
@@ -0,0 +1,12 @@
+create table Supplier (
+   Id                   int                  identity,
+   CompanyName          nvarchar(40)         not null,
+   ContactName          nvarchar(50)         null,
+   ContactTitle         nvarchar(40)         null,
+   City                 nvarchar(40)         null,
+   Country              nvarchar(40)         null,
+   Phone                nvarchar(30)         null,
+   Fax                  nvarchar(30)         null,
+   constraint PK_SUPPLIER primary key (Id)
+)
+go
