e5080dc7bacbe7027c5a47cf4f885fe06abee8d5
Vinicius Ribeiro <87951323+ribeiro-vs@users.noreply.github.com>
2022-06-28 00:22:03 -0300

    Create customer.sql

---
+++ Tables/customer.sql
@@ -0,0 +1,10 @@
+create table Customer (
+   Id                   int                  identity,
+   FirstName            nvarchar(40)         not null,
+   LastName             nvarchar(40)         not null,
+   City                 nvarchar(40)         null,
+   Country              nvarchar(40)         null,
+   Phone                nvarchar(20)         null,
+   constraint PK_CUSTOMER primary key (Id)
+)
+go
