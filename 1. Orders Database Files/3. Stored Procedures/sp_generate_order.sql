fea1b9300b43323df2603d97aa0b1efa37c99ca8
Vinicius Ribeiro <87951323+ribeiro-vs@users.noreply.github.com>
2022-09-26 18:00:28 -0300

    Update sp_generate_order.sql

--- 1. Orders Database Files/3. Stored Procedures/sp_generate_order.sql
+++ 1. Orders Database Files/3. Stored Procedures/sp_generate_order.sql
@@ -1,74 +1,73 @@
 --CREATE PROCEDURE sp_generate_order
 --AS
 --  BEGIN
+DECLARE @customer_id INT = 1
+DECLARE @product_id INT = 1
+DECLARE @quantity INT = 1
+DECLARE @orderdate DATETIME = Getutcdate()
+DECLARE @totalamount INT 
 
-      DECLARE @customer_id int  = 1
-      DECLARE @product_id  int  = 1
-      DECLARE @quantity    int  = 1
-	  DECLARE @orderdate   datetime = getdate()
-	  DECLARE @totalamount int 	--bring this value
- 
-	  DECLARE @order_item   table
-	  (OrderId  int,
-	  ProductId int,
-	  UnitPrice int,
-	  Quantity  int)
+DECLARE @order_item TABLE
+  (
+     orderid   INT,
+     productid INT,
+     unitprice INT,
+     quantity  INT
+  )
+DECLARE @order TABLE
+  (
+     orderdate   DATE,
+     ordernumber INT,--see how it is in the ddl query
+     customerid  INT,
+     totalamount INT
+  )
 
-	  DECLARE @order table
-	  (OrderDate  date,
-	  OrderNumber int, --see how it is in the ddl query
-	  CustomerId  int,
-	  TotalAmount int)
+BEGIN TRAN
 
-      BEGIN TRAN
-
-	  declare @unitprice int
+DECLARE @unitprice INT
 
 --1	  --bring unit price and other values to oi / not order_id
- 	  insert into @order_item
-	  select null,
-			 @product_id, 
-			 UnitPrice, 
-			 @quantity 
-	  from Product
-		--cross apply(select OrderNumber from @order) o
-	  where Id = @product_id
-	  select * from @order_item
+INSERT INTO @order_item
+SELECT NULL,
+       @product_id,
+       unitprice,
+       @quantity
+FROM   product
+WHERE  id = @product_id
 
+--test
+SELECT *
+FROM   @order_item
 
---2	  --trying to bring ordernumber formatted
-	  insert into @order  
-	  select top 1 @orderdate, OrderNumber+1 as new_ordernumber, @customer_id, oi.total
-	  from [order] 
-		cross apply (select UnitPrice*Quantity as total from @order_item) oi
-	  order by new_ordernumber desc
+--2	  --trying to bring ordernumber formatted into order
+INSERT INTO @order
+SELECT TOP 1 @orderdate,
+             ordernumber + 1 AS new_ordernumber,
+             @customer_id,
+             oi.total
+FROM   [order]
+       CROSS apply (SELECT unitprice * quantity AS total
+                    FROM   @order_item) oi
+ORDER  BY new_ordernumber DESC
 
-	  select * from @order
-	  select top 1 OrderNumber+1
-	  from [order] 
-	  order by 1 desc
+--test
+SELECT *
+FROM   @order
+--test
+SELECT TOP 1 ordernumber + 1
+FROM   [order]
+ORDER  BY 1 DESC
 
 --3 --bring unit price and other values to oi 
-	  
-	  update @order_item
-	  set OrderId = OrderNumber from @order
+UPDATE @order_item
+SET    orderid = ordernumber
+FROM   @order
 
-	  select * from @order_item
-	    
-      --INSERT INTO @order_item
-      --VALUES      (--orderid
-				  -- @product_id,
-				  -- @unitprice,
-      --             @quantity)
+--test
+SELECT *
+FROM   @order_item
+--test
+select format(@orderdate, 'yyyy/mm/dd, hh:mm:ss')--add utc -3
 
-      --INSERT INTO @order
-      --VALUES      (--orderid
-				  -- @product_id,
-				  -- @customer_id,
-      --             @quantity)
-				   
-      COMMIT TRAN
-  --END 
-
-   
-
+COMMIT TRAN
+--END 
